8696f978320085716c31bd4dc672d9ba9567f291
Milardez <milardez@icloud.com>
2018-11-05 22:42:56 +0300

    task 9

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
+ var signinbutton = document.getElementById('joinnewsletter');
+ console.log (signinbutton);
 
-var X ;
-var Y ;
-X= prompt ("Задайте величину X");
-console.log(X);
-Y= prompt ("Задайте величину Y");
-console.log (Y);
-var SUM = X + Y; 
-alert(SUM);
+ function checkEvent(event){
+    
+    event.preventDefault();
+	console.log ("Нажали")};
+
+ signinbutton.addEventListener ("click", checkEvent);
+
+ function checkEvent(event){
+    
+    event.preventDefault();
+
+var yourname = document.getElementById("yourname");
+var valueyourname = yourname.value;
+
+if (valueyourname === '' ){
+
+yourname.style.border = '2px solid red';	
+	console.log('Введите имя');
+} else {
+	yourname.style.border = '1px solid gray';
+}
+
+var yourEmail = document.getElementById("yourEmail");
+var valueyourEmail = yourEmail.value;
+
+if (valueyourEmail === '' ){
+
+yourEmail.style.border = '2px solid red';	
+	console.log('Введите действительный email');
+} else {
+	yourEmail.style.border = '1px solid gray';
+} }
+
+var thankyoumessage = document.getElementById('thankyoumessage');
+console.log (thankyoumessage);
+
+thankyoumessage.addEventListener ('submit', checkEvent);
+
+function checkEvent(event){
+
+if ('submit') { 
+event.thankyoumessage();
+thankyoumessage.style.display = " "}                                   
+
+
